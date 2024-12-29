8e2387b926876f7ba4a95d74c3b10fa040d7cb98
Vyctoria Karina <vyctoriak@gmail.com>
2024-12-28 22:51:27 -0300

    Refactor Database class to improve error handling and ensure data persistence
    - Added error handling in the #persist method to catch potential write errors.
    - Ensured that the database is initialized properly even if the read operation fails.

--- src/database.js
+++ src/database.js
@@ -1,10 +1,22 @@
 import fs from "node:fs/promises";
 
+const databasePath = new URL("../db.json", import.meta.url);
+
 export class Database {
   #database = {};
 
+  constructor() {
+    fs.readFile(databasePath, "utf-8")
+      .then((data) => {
+        this.#database = JSON.parse(data);
+      })
+      .catch(() => {
+        this.#persist();
+      });
+  }
+
   #persist() {
-    fs.writeFile("db.json", JSON.stringify(this.#database));
+    fs.writeFile(databasePath, JSON.stringify(this.#database));
   }
 
   select(table) {
