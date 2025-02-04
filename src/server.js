570deb4b55d88fcf9d50f9a17a6fb550981f846c
Vyctoria Karina <vyctoriak@gmail.com>
2025-02-04 18:22:56 -0300

    feat: filtering query params

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import http from "node:http";
 import { json } from "./middlewares/json.js";
 
 import { routes } from "./routes.js";
+import { extractQueryParams } from "./utils/extract-query-params.js";
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req;
@@ -14,6 +15,12 @@ const server = http.createServer(async (req, res) => {
 
   if (route) {
     const routeParams = req.url.match(route.path)
+
+    const { query, ...params } = routeParams.groups
+    
+    req.params = params
+    req.query = query ? extractQueryParams(query) : {}
+
     req.params = { ...routeParams.groups}
     return route.handler(req, res);
   }
